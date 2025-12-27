29777b62e15f910c5d7bcb817da9d524e46722c4
Игорь <112966801+SmallUpdate@users.noreply.github.com>
2025-12-27 15:17:51 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 canvas.width =  400;
 canvas.height = 400;
 
-document.body.style.backgroundColor = "#121212"
-document.body.style.margin = 0
-canvas.style.position = "absolute"
-canvas.style.left = "50%"
-canvas.style.top = "50%"
-canvas.style.transform = "translate(-50%, -50%)"
+const CLR1 = "#121212";
+const CLR2 = "#242424";
+const CLR3 = "#8A8A8A";
+const CLR4 = "#FFFFFF";
+const CLR5 = "#BC13FE";
+
+document.body.style.backgroundColor = CLR1;
+document.body.style.margin = 0;
+canvas.style.position = "absolute";
+canvas.style.left = "50%";
+canvas.style.top = "50%";
+canvas.style.transform = "translate(-50%, -50%)";
 
 const ctx = canvas.getContext("2d");
 
@@ -14,96 +20,96 @@ ctx.font = "48px Consolas";
 ctx.textBaseline = "middle";
 ctx.textAlign = "center";
 
-// const ds = [[-1, 0], [1, 0], [0, 1], [0, -1]];
-const ds = [[1, 0], [0, 1], [-1, 0], [0, -1]]
-let m = [
-    [ 1,  2,  3,  4],
-    [ 5,  6,  7,  8],
-    [ 9, 10, 11, 12],
-    [13, 14, 15,  0]
-]
-let p = [3, 3]
+const size = 4;
+let matrix = generateMatrix(4);
+let player = {x: matrix.length-1, y: matrix[0].length-1};
+const directions = [[0,-1], [-1,0], [0,1], [1,0]];
+randomizeMatrix(matrix);
+drawMatrix(matrix);
 
-function randomMove() {
-    pd = []
-        for (let j = 0; j < 4; ++j) {
-            const nx = p[0] + ds[j][0]
-            const ny = p[1] + ds[j][1]
-            if (0<=nx && nx<4 && 0<=ny && ny<4) {
-                pd.push(ds[j])
-            }
+function generateMatrix(size) {
+    let matrix = [];
+    for (let i = 0; i < size; i++) {
+        row = [];
+        for (let j = 0; j < size; j++) {
+            row.push(i == size-1 && j == size-1 ? 0 : i*size + j+1);
         }
-        j = Math.floor(Math.random() * pd.length);
-        const d = pd[j];
-        const nx = p[0]+d[0];
-        const ny = p[1]+d[1];
-        m[p[1]][p[0]] = m[ny][nx];
-        m[ny][nx] = 0;
-        p = [nx, ny];
-}
-
-function checkCorrect(x, y, n) {
-    return n-1 == x+4*y
-}
-
-function drawCell(x, y, n) {
-    if (n) {
-        const s = 94;
-        const g =  8;
-        const b = checkCorrect(x, y, n)
-        ctx.fillStyle = b ? "#BC13FE" : "#242424";
-        ctx.fillRect(x*(s+g), y*(s+g), s, s);
-        ctx.fillStyle = b ?  "#FFFFFF": "#8A8A8A"
-        ctx.fillText(n, x*(s+g)+s/2, y*(s+g)+s/2)
+        matrix.push(row);
     }
+    return matrix;
 }
 
 function clear() {
-    ctx.fillStyle = "#121212"
-    ctx.fillRect(0, 0, canvas.width, canvas.height)
+    ctx.fillStyle = CLR1;
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
 }
 
-function drawField() {
+function drawMatrix(matrix) {
     clear()
-    for (let i = 0; i < 4; ++i) {
-        for (let j = 0; j < 4; ++j) {
-            drawCell(j, i, m[i][j]);
+    for (let i = 0; i < matrix.length; i++) {
+        for (let j = 0; j < matrix[0].length; j++) {
+            drawElement(j, i, matrix[i][j], matrix.length);
         }
     }
 }
 
-drawField();
+function drawElement(j, i, value, size) {
+    if (value) {
+        const cellSize = 100;
+        const padding = 4;
+        const [x, y] = [cellSize*j, cellSize*i];
+        const b = i*size+j+1 == value
+        ctx.fillStyle = b ? CLR5 : CLR2;
+        ctx.fillRect(x+padding, y+padding, cellSize-2*padding, cellSize-2*padding);
+        ctx.fillStyle = b ? CLR4 : CLR3;
+        ctx.fillText(value, x+cellSize/2, y+cellSize/2);
+    }
+}
+
+function move(x, y) {
+    matrix[player.y][player.x] = matrix[y][x];
+    matrix[y][x] = 0;
+    player = {x, y};
+    drawMatrix(matrix);
+}
+
+function click(e) {
+    if (e.target == canvas) {
+    const x = Math.floor(e.layerX / canvas.width * size);
+    const y = Math.floor(e.layerY / canvas.height * size);
+        if (Math.abs(player.x-x) + Math.abs(player.y-y) == 1) {
+            move(x, y);
+        }
+    }
+}
 
 function keydown(e) {
-    // console.log(e)
-    c = e.keyCode;
-    //   D   S   A   W 
-    i = [68, 83, 65, 87].indexOf(c)
-    // i = [65, 87, 68, 83].indexOf(c)
+    const i = [87, 65, 83, 68].indexOf(e.keyCode);
     if (i >= 0) {
-        const d = ds[i]
-        const nx = p[0] + d[0]
-        const ny = p[1] + d[1]
-        if (0<=nx && nx<4 && 0<=ny && ny<4) {
-            m[p[1]][p[0]] = m[ny][nx]
-            m[ny][nx] = 0
-            p = [nx, ny]
-            drawField()
+        const [dx, dy] = directions[i];
+        const [nx, ny] = [player.x + dx, player.y + dy];
+        if (0<=nx && nx<size && 0<=ny && ny<size) {
+            move(nx, ny);
         }
     }
 }
 
-let moves = 0
-const FPS = 60
-
-function resolve() {
-    randomMove()
-    drawField()
-    moves += 1
-    if (moves < 100) {
-        setTimeout(resolve, 1000/FPS)
-    } else {
-        window.addEventListener('keydown', keydown);
+function randomizeMatrix(matrix) {
+    for (let i = 0; i < 2020; i++) {
+        let possibleMovements = [];
+        for (const [dx, dy] of directions) {
+            const [nx, ny] = [player.x + dx, player.y + dy];
+            if (0<=nx && nx<size && 0<=ny && ny<size) {
+                possibleMovements.push([nx, ny]);
+            }
+        }
+        const [x, y] = possibleMovements[Math.floor(Math.random() * possibleMovements.length)];
+        matrix[player.y][player.x] = matrix[y][x];
+        matrix[y][x] = 0;
+        player = {x, y};
     }
+    drawMatrix(matrix)
 }
-resolve()
+
+window.addEventListener("click", click);
+window.addEventListener("keydown", keydown);
